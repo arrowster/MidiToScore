ed55618818c6376452bcb007750eed366e3a88e6
arrowster <kjy9835@naver.com>
2023-04-13 15:00:28 +0900

    components(DragDropBox,MusicScoreViewer) 구현

--- MidiToScore/src/components/MusicScoreViewer.vue
+++ MidiToScore/src/components/MusicScoreViewer.vue
@@ -1,16 +1,53 @@
-// MusicScoreViewer    - 최종 수정 : 23/04/00 00:00 ㅇㅇㅇ -
+// MusicScoreViewer    - 최종 수정 : 23/04/13 14:59 김지용 -
+
 <template>
   <v-container>
-
+    <div class="justify-center">
+      <v-btn class="ma-4" @click="pagePrev">Prev</v-btn>
+      <div class="mt-6"> {{currentPage}} / {{pages}} </div>
+      <v-btn class="ma-4" @click="pageNext">Next</v-btn>
+    </div>
+    <div class="justify-center">
+      <VuePDF :pdf="pdf" :page="currentPage" />
+    </div>
   </v-container>
 </template>
 
 <script>
+import { usePDF, VuePDF } from '@tato30/vue-pdf'
+import { ref } from 'vue'
+
 export default {
-  name: "MusicScoreViewer"
+  name: "MusicScoreViewer",
+
+  components: {
+    VuePDF
+  },
+  setup() {
+    let pdfUrl = "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
+    const currentPage = ref(1)
+    const { pdf, pages } = usePDF(pdfUrl)
+    return {
+      pdf,
+      pages,
+      currentPage
+    }
+  },
+  methods: {
+    pageNext() {
+      this.currentPage = this.currentPage < this.pages ? this.currentPage + 1 : this.pages
+      console.log(this.currentPage)
+    },
+    pagePrev() {
+      this.currentPage = this.currentPage > 1 ? this.currentPage - 1 : 1
+      console.log(this.currentPage)
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-
+.justify-center{
+  display: flex;
+}
 </style>
--- MidiToScore/src/views/Home.vue
+++ MidiToScore/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
-  <HelloWorld />
+  <DragDropBox />
+  <MusicScoreViewer />
 </template>
 
 <script setup>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import MusicScoreViewer from '@/components/MusicScoreViewer.vue'
+  import DragDropBox from "@/components/DragDropBox";
 </script>
