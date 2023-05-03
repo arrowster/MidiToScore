1fb5ffc5a0a344d511b75e920971d2282d69e3f1
arrowster <kjy9835@naver.com>
2023-05-03 22:45:52 +0900

    backend __dirname오류 긴급수정

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import createError from 'http-errors';
 import express from 'express';
-/*import path from 'path';*/
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv'
@@ -9,20 +9,19 @@ dotenv.config()
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import managerRouter from "./routes/manager.js";
-import manager from "./routes/manager.js";
-
 
 const app = express();
+const __dirname = path.resolve();
 
 // view engine setup
-/*app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');*/
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-/*app.use(express.static(path.join(__dirname, 'public')));*/
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
